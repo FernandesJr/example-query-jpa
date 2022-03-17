e0d9ccc81e3467b4a28f0e029f13d0672c12bb21
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-03-17 14:38:25 -0300

    uri2609 SQL and JPQL with inner join and group by

--- uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2609;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2609.repositories.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2609Application implements CommandLineRunner {
@@ -20,5 +25,20 @@ public class Uri2609Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		List<CategorySumProjection> list = repository.search01();
+		List<CategorySumDTO> result01 = list.stream().map(x -> new CategorySumDTO(x)).collect(Collectors.toList());
+
+		System.out.println("*** SQL RAIZ ***");
+		for(CategorySumDTO d : result01){
+			System.out.println(d);
+		}
+		System.out.println("\n");
+
+		List<CategorySumDTO> result02 = repository.search02();
+
+		System.out.println("*** SQL JPQL ***");
+		for(CategorySumDTO d : result02){
+			System.out.println(d);
+		}
 	}
 }
