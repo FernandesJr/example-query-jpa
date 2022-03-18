49c511ee935a7813ce5ecba58d4cf5cb96707aa8
Fernandes Júnior <fernaandes.jr@gmail.com>
2022-03-18 11:56:52 -0300

    uri2737 query sql with UNION

--- uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
@@ -1,9 +1,23 @@
 package com.devsuperior.uri2737.repositories;
 
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2737.entities.Lawyer;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LawyerRepository extends JpaRepository<Lawyer, Long> {
+
+    //Tem que apelidar o customers_number pelo fato de ser snake_case
+    @Query(nativeQuery = true, value = "SELECT name, customers_number AS CustomersNumber FROM lawyers " +
+            "WHERE customers_number = (SELECT MAX(customers_number) FROM lawyers) " +
+            "UNION ALL " +
+            "SELECT name, customers_number FROM lawyers " +
+            "WHERE customers_number = (SELECT MIN(customers_number) FROM lawyers) " +
+            "UNION ALL " +
+            "SELECT 'Avegare', ROUND(AVG(customers_number)) FROM lawyers")
+    List<LawyerMinProjection> search01(); //Até então a JPA não oferece UNION
 
 }
